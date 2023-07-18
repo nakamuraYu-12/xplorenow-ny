21b4031a6ce57e6fc3978c7d46b6eaef44d28559
NakamuraY <nakamuray.pg@gmail.com>
2023-07-19 03:52:01 +0900

    ユーザー名表示、ゲストユーザーにも対応

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_12_150436) do
+ActiveRecord::Schema.define(version: 2023_07_18_180616) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2023_07_12_150436) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "profile"
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
