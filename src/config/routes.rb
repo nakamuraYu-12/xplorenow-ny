b5ffc59aa453b50d287b0f8195a0dfb2aeba4b11
NakamuraY <nakamuray.pg@gmail.com>
2023-07-21 03:44:02 +0900

    新規登録画面作成,devise日本語化

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'top#index'
-  get '/', to: 'top#index'
-
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'top#index'
+  get '/', to: 'top#index'
+
 end
