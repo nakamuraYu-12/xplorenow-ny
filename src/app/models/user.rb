77d57b1b27eb0983b5858025da7a50e2bbe1e779
NakamuraY <nakamuray.pg@gmail.com>
2023-07-30 02:30:14 +0900

    userバリデーション追加、デザイン修正

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
+  validates :name, length: { minimum: 1, maximum: 30 }
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
