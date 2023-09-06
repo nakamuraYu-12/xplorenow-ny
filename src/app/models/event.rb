59126ed78e472f034dcfb086dd577e5a5cdcc80f
NakamuraY <nakamuray.pg@gmail.com>
2023-09-06 19:43:36 +0900

    create時にバリデーションがうご買いため、新たにバリデーション追加

--- src/app/models/event.rb
+++ src/app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :address, presence: true
-  validate :event_dates_presence, on: :update
+  validate :event_dates_presence
+  validate :event_dates_update_presence
   validate :event_dates_limit3
   mount_uploader :image, ImageUploader
   has_many :event_dates, dependent: :destroy
@@ -15,11 +16,14 @@ class Event < ApplicationRecord
   before_save :remove_image_flagged, if: :remove_image_flagged
 
   def event_dates_presence
+    errors.add(:base, "イベント開催日時を設定してください") if event_dates.none?
+  end
+
+  def event_dates_update_presence
     if event_dates.reject(&:marked_for_destruction?).empty?
       errors.add(:base, "イベント開催日時を設定してください")
     end
   end
-
   def event_dates_limit3
     errors.add(:base, "イベント開催日の登録は３日までにしてください") if event_dates.size > 3
   end
